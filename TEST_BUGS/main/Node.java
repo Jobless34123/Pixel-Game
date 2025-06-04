c0f25fe99908a3c88bf64d761aac411cd44593c6
AarizMF <aariz.farooqui@student.tdsb.on.ca>
2025-06-03 21:50:55 -0400

    Fixed bugs + zombies
    look at build handler
    added zombies, pathfinder and UI

---
+++ TEST_BUGS/main/Node.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Node {
+    Node parent;
+    public int col, row;
+    int gCost, hCost, fCost;
+    boolean solid, open, checked;
+
+    public Node (int col, int row){
+        this.row=row;
+        this.col=col;
+    }
+    
+}
---
+++ TEST_BUGS/tile/Tile.java
@@ -0,0 +1,7 @@
+//This is the Tile object that loads every tile
+package tile;
+import java.awt.image.BufferedImage;
+public class Tile {
+    public BufferedImage image;
+    public boolean collision = false;
+}
