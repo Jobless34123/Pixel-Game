53e81fa51a48462137c76319b0a2219b3cec234a
Jobless34123 <moiz.341239@gmail.com>
2025-06-10 12:59:49 -0400

    Delete src directory

--- src/tile/Tile.java
+++
@@ -1,7 +0,0 @@
-//This is the Tile object that loads every tile
-package tile;
-import java.awt.image.BufferedImage;
-public class Tile {
-    public BufferedImage image;
-    public boolean collision = false;
-}
